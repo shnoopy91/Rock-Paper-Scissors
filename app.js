9682615bcf99d8cabb0f6eb143467a5c9d9ee34d
shnoopy91 <120331526+shnoopy91@users.noreply.github.com>
2023-11-06 19:31:45 -0600

    Completed - Rock Paper Scissors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let roundsPlayed = 1;
 const rounds = document.querySelector('#rounds');
 const output = document.querySelector('#output');
 let score = document.querySelector('#score');
+const reset = document.querySelector('#resetGame');
 
     function computerSelection() {
         let computerChoice = Math.floor(Math.random() * 3);
@@ -73,7 +74,7 @@ let score = document.querySelector('#score');
             };
         
             rounds.textContent = `Round: ${roundsPlayed}`;
-            roundsPlayed++;
+            roundsPlayed++
                 if (playerScore === 5) {
                     endGame()
                 }
@@ -85,16 +86,21 @@ let score = document.querySelector('#score');
     
         function endGame() {
             if (playerScore === 5) {
-                output.textContent = 'You win the game! Refresh to play again.';
+                output.textContent = 'You win the game! Restart to play again.';
             }
             else if (computerScore === 5) {
-                output.textContent = 'You lose the game! Refresh to play again.';
-            } 
-            else {
-            output.textContent = 'It\'s a draw!';
+                output.textContent = 'You lose the game! Restart to play again.';
             };
+            
             updateScore();
         };
+        reset.onclick = () => 
+            {rounds.textContent = `Round: ${roundsPlayed = 0}`;
+            score.textContent = `Player: ${playerScore = 0} | Computer: ${computerScore = 0}`;
+            output.textContent = 'No move has been made yet';
+        roundsPlayed++;
+        updateScore();
+    };
 
 
         
